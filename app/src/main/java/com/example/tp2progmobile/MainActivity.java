c1e20ae7fe28d27d956b26142a69a717409e8574
Abdkar <abdoul2karym@gmail.com>
2021-03-23 15:02:29 +0100

    commit

---
+++ app/src/main/java/com/example/tp2progmobile/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.tp2progmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button envoyer = null;
+    Button reset = null;
+    EditText taille = null;
+    EditText poids = null;
+    CheckBox commentaire = null;
+    RadioGroup group = null;
+    TextView result = null;
+
+    private final String texteInit = "Cliquez sur le bouton « Calculer l'IMC » pour obtenir un résultat.";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // On récupère toutes les vues dont on a besoin
+        envoyer = (Button)findViewById(R.id.calcul);
+        reset = (Button)findViewById(R.id.reset);
+        taille = (EditText)findViewById(R.id.taille);
+        poids = (EditText)findViewById(R.id.poids);
+        commentaire = (CheckBox)findViewById(R.id.commentaire);
+        group = (RadioGroup)findViewById(R.id.group);
+        result = (TextView)findViewById(R.id.result);
+
+        // On attribue un listener adapté aux vues qui en ont besoin
+        envoyer.setOnClickListener(envoyerListener);
+        reset.setOnClickListener(resetListener);
+        commentaire.setOnClickListener(checkedListener);
+        //taille.addTextChangedListener(textWatcher);
+        //poids.addTextChangedListener(textWatcher);
+        taille.setOnKeyListener(modifListener);
+        poids.setOnKeyListener(modifListener);
+
+    }
+    private View.OnClickListener envoyerListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            //  on récupère la taille
+            String t = taille.getText().toString();
+            // On récupère le poids
+            String p = poids.getText().toString();
+            float tValue = Float.valueOf(t);
+
+            if (t.contains("    .")) {
+                //Utiliser setChecked(boolean checked) ou toggle()
+                //test.setChecked(false);
+                //Log.i("", "state chenged, new state = " + test.isChecked());
+            }
+
+            // Puis on vérifie que la taille est cohérente
+            if(tValue <= 0)
+                Toast.makeText(MainActivity.this, "La taille doit être positive", Toast.LENGTH_SHORT).show();
+            else {
+                float pValue = Float.valueOf(p);
+                if(pValue <= 0)
+                    Toast.makeText(MainActivity.this, "Le poids doit etre positif", Toast.LENGTH_SHORT).show();
+                else {
+                    // Si l'utilisateur a indiqué que la taille était en centimètres
+                    // On vérifie que la Checkbox sélectionnée est la deuxième à l'aide de son identifiant
+                    if (group.getCheckedRadioButtonId() == R.id.radio_centimetre) tValue = tValue / 100;
+                    float imc = pValue / (tValue * tValue);
+                    String resultat="Votre IMC est " + imc+" . ";
+
+                    if(commentaire.isChecked()) resultat += interpreteIMC(imc);
+
+                    result.setText(resultat);
+                }
+            }
+        }
+        public String interpreteIMC(float i) {
+            if (i < 16.5) {
+                return "\nFamine";
+            }
+            else if (i >= 16.5 && i < 18.5) {
+                return "\nMaigreur";
+            }
+            else if (i >= 18.5 && i < 25) {
+                return "\nCorpulence normale";
+            }
+            else if (i >= 25 && i < 30) {
+                return "\nSurpoids";
+            }
+            else if (i >= 30 && i < 35) {
+                return "\nObésité modérée";
+            }
+            else if (i >= 35 && i < 40) {
+                return "\nObésité sévère";
+            }
+            return "\nObésité morbide ou sévère";
+        }
+    };
+
+    private View.OnClickListener resetListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            poids.getText().clear();
+            taille.getText().clear();
+            result.setText(texteInit);
+        }
+    };
+
+    private View.OnClickListener checkedListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            if(((CheckBox)v).isChecked()) {
+                result.setText(texteInit);
+            }
+        }
+    };
+
+    private View.OnKeyListener modifListener = new View.OnKeyListener() {
+
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            // On remet le texte à sa valeur par défaut
+            result.setText(texteInit);
+            return false;
+        }
+    };
+
+    private TextWatcher textWatcher = new TextWatcher() {
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            result.setText(texteInit);
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        @Override
+        public void afterTextChanged(Editable s) {}
+
+    };
+}
